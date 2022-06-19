991c82377dbcebb6172fc8b7df5513efeec23cda
Mais1212 <64730510+Mais1212@users.noreply.github.com>
2022-06-19 21:56:18 +0300

    Add preview for images

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,34 @@
-from turtle import title
+from turtle import width
 from django.contrib import admin
-from .models import Place, Image
+from django.utils.html import format_html
+
+from .models import Image, Place
 
 # Register your models here.
 
 
-admin.site.register(Place,)
-admin.site.register(Image,)
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+
+class ImageInline(admin.TabularInline):
+    def headshot_image(self, image):
+        html = format_html(
+            """<img src="{}" width="{}" height={} />""",
+            image.image.url,
+            200,
+            200
+        )
+
+        return html
+
+    readonly_fields = ["headshot_image"]
+    model = Image
+    fields = (("image",  "headshot_image"))
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
+    list_display = ["title", "place_id"]
